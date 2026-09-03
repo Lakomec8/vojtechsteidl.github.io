b6e4174371c728174a3c13ccae9f900cfd127c31
Lakomec8 <donnytorriem@gmail.com>
2026-09-03 16:31:44 +0200

    Ensure Sep 2 calendar participants exist

---
+++ cloudflare-portal/migrations/0014_fix_sep2_calendar_participants.sql
@@ -0,0 +1,5 @@
+PRAGMA foreign_keys = ON;
+
+INSERT OR IGNORE INTO tutoring_calendar_event_students (google_event_id, student_id) VALUES
+  ('0uon2ms175g3mbjat55jbatr9g_20260902T140000Z', 'vojta'),
+  ('63qubsrgpd8rvh8vvqac8dkca5', 'anicka');
